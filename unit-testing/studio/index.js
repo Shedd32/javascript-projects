bee0a6c4ea5a2e6bbd6cef154725b8da838230c4
Shedd32 <justinshedd21@gmail.com>
2023-10-09 23:30:21 -0500

    studios

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,27 @@
 
 let launchcode = {
-  
+
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(num){
+    let output = ""
+    if(num % 2 === 0){
+        output += "Launch";
+    }
+    if (num % 3 === 0){
+        output += "Code";
+    }
+    if (num % 5 === 0){
+        output += " ";
+    }
+        output += "Rocks";
+    if (output.length === 0){
+        return "Rutabagas! That doesn't work."
+    }    
+        return output = "!"
+  }
 }
 
 module.exports = launchcode;
